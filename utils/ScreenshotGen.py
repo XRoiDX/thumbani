c1bdc24f348df2a7a42194519d611d5dac8f7c66
X Roid <97869723+XRoiDX@users.noreply.github.com>
2025-08-24 08:10:15 +0530

    Update ScreenshotGen.py

--- utils/ScreenshotGen.py
+++ utils/ScreenshotGen.py
@@ -102,7 +102,7 @@ def convertToScreenshot2(url):
 
 def get_screenshot(episodeid, large):
     url = (
-        f"https://animedexapi-private-124143.techzbots1.workers.dev/episode/{episodeid}"
+        f"https://usefull-ani-api1.theconquet11.workers.dev/episode/{episodeid}"
     )
     data = get_json(url)
     url = data["results"]["stream"]["sources"][0]["file"]
